ea51e4161de3bfc9d742483de3ce6896f10a936b
stormtomten <80994025+stormtomten@users.noreply.github.com>
2025-11-05 19:42:38 +0100

    A faulty test

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,58 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	tests := []struct {
+		name        string
+		inputHeader http.Header
+		wantString  string
+		wantError   error
+	}{
+		{
+			name: "Correct",
+			inputHeader: http.Header{
+				"Authorization": {"ApiKey Atoken"},
+			},
+			wantString: "Atoken",
+			wantError:  nil,
+		},
+		{
+			name: "Empty Auth header",
+			inputHeader: http.Header{
+				"Authorization": {},
+			},
+			wantString: "",
+			wantError:  ErrNoAuthHeaderIncluded,
+		},
+		{
+			name: "Malformed header",
+			inputHeader: http.Header{
+				"Authorization": {"Malformation is I"},
+			},
+			wantString: "",
+			wantError:  errors.New("malformed authorization header"),
+		},
+		{
+			name: "Many splits",
+			inputHeader: http.Header{
+				"Authorization": {"ApiKey To many splits"},
+			},
+			wantString: "To",
+			wantError:  errors.New("malformed authorization header"),
+		},
+	}
+
+	for _, tc := range tests {
+		got, err := GetAPIKey(tc.inputHeader)
+		if !reflect.DeepEqual(tc.wantString, got) || !reflect.DeepEqual(tc.wantError, err) {
+			t.Fatalf("expected: %v;%v, got: %v;%v", tc.wantString, tc.wantError, got, err)
+		}
+
+	}
+}
